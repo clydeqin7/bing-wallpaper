5e56c40a886f9cccfd2275edf2a469475b7a1a9b
clydeqin7 <clydeqin7@foxmail.com>
2025-02-13 10:48:47 +0800

    build: 依赖更新，ts类型完善

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import axios from 'axios';
 
 export function saveImage(url: string, filePath: string): Promise<void> {
   return new Promise((resolve, reject) => {
